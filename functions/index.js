0e7a2ad45f38259249e917788cec58c10c810ad0
Deepanshu Bisht <deepanshubisht188@gmail.com>
2025-03-05 04:19:42 +0530

    added: verifyPayment method

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,15 @@
 import functions from "firebase-functions";
 import Razorpay from "razorpay";
 import cors from "cors";
+import 'dotenv/config';
+import crypto from 'node:crypto';
+
+
+const rzp_secret = process.env.RAZORPAY_TEST_KEY_SECRET;
 
 const razorpay = new Razorpay({
-  key_id: import.meta.env.VITE_RAZORPAY_TEST_KEY_ID,
-  key_secret: import.meta.env.VITE_RAZORPAY_TEST_KEY_SECRET,
+  key_id: process.env.RAZORPAY_TEST_KEY_ID,
+  key_secret: rzp_secret,
 });
 
 export const createOrder = functions.https.onRequest((req, res) => {
@@ -27,5 +32,39 @@ export const createOrder = functions.https.onRequest((req, res) => {
       console.error(error);
       res.status(500).json({ error: "Failed to create Razorpay order" });
     }
+  });
+});
+
+export const verifyPayment = functions.https.onRequest(async (req, res) => {
+  cors({ origin: true })(req, res, async () => {
+    if (req.method !== "POST") {
+      return res.status(400).json({ message: "Invalid request method" });
+    }
+    const { razorpay_payment_id, razorpay_signature } = req.body;
+    // order id need to be fetched from the database
+    const orderId = 'order_rcptid_11'
+
+    function verifySignature() {
+      // Construct HMAC payload
+      const payload = `${orderId}|${razorpay_payment_id}`;
+
+      // Generate HMAC hex digest
+      const generatedSignature = crypto
+        .createHmac('sha256', rzp_secret)
+        .update(payload)
+        .digest('hex');
+
+      // Secure comparison (prevents timing attacks)
+      return crypto.timingSafeEqual(
+        Buffer.from(generatedSignature, 'utf8'),
+        Buffer.from(razorpay_signature, 'utf8')
+      );
+    }
+
+    const isValid = verifySignature()
+    if (!isValid) {
+      return res.status(401).json({ message: "Invalid signature", status: 401 });
+    }
+    res.status(200).json({ message: "Payment verified successfully", status: 200 });
   });
 });
