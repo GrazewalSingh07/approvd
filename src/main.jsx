63cac40e436f6ee72058c22cd52e9750ecbd2bb2
Deepanshu Bisht <deepanshubisht188@gmail.com>
2025-03-05 04:20:54 +0530

    added: react-hot-toast package

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ConfigProvider,theme } from 'antd'
+import { ConfigProvider, theme } from 'antd'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <BrowserRouter>
-   <ConfigProvider
-    theme={{
-     
-      algorithm: theme.darkAlgorithm,
- 
-    }}
-  >
-    <App />
-  </ConfigProvider>
-  </BrowserRouter>
-    
+    <Toaster />
+    <BrowserRouter>
+      <ConfigProvider
+        theme={{
+
+          algorithm: theme.darkAlgorithm,
+
+        }}
+      >
+        <App />
+      </ConfigProvider>
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
